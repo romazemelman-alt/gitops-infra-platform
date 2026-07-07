f525996b1681a176c368c0b0413cf61cc621d40a
regis <regis7374022@gmail.com>
2026-07-07 16:14:49 +0300

    add flow for infra creation

--- terraform/modules/eks/variables.tf
+++ terraform/modules/eks/variables.tf
@@ -2,7 +2,7 @@ variable "environment" { type = string }
 variable "cluster_name" { type = string }
 variable "cluster_version" {
   type    = string
-  default = "1.30"
+  default = "1.32"
 }
 variable "vpc_id" { type = string }
 variable "private_subnet_ids" { type = list(string) }
